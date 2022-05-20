dc700ca7f19f5010a2684c8ba6ac9543de6ff039
Abednego Bogi Christian <99196129+AbogiC@users.noreply.github.com>
2022-05-20 11:49:06 +0700

    edit controller

--- src/main/java/v3aplikasi/controller/EmployeeController.java
+++ src/main/java/v3aplikasi/controller/EmployeeController.java
@@ -1,6 +1,7 @@
 package v3aplikasi.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,10 +11,61 @@ import v3aplikasi.model.dto.EmployeeDto;
 import v3aplikasi.model.entity.EmployeeEntity;
 import v3aplikasi.service.EmployeeService;
 
+import javax.servlet.http.HttpServletRequest;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
 @RestController
 @RequestMapping("/employeedao")
 public class EmployeeController {
     @Autowired
     private EmployeeService employeeService;
 
+    @GetMapping("/all")
+    public String home(Model model, HttpServletRequest request){
+        request.setAttribute("action", "insert");
+        if(request.getParameter("id") != null){
+            try{
+                EmployeeEntity employeeEntity = new EmployeeEntity();
+                employeeEntity = employeeService.getEmployee(Integer.parseInt(request.getParameter("id")));
+                request.setAttribute("name", employeeEntity.getName());
+                request.setAttribute("birth_date", employeeEntity.getBirth_date());
+                request.setAttribute("position_id", employeeEntity.getPositionID());
+                request.setAttribute("id_number", employeeEntity.getId_number());
+                request.setAttribute("gender", employeeEntity.getGender());
+            } catch (Exception e){
+                //TODO Auto-generated catch block
+                e.printStackTrace();
+                request.setAttribute("errorMessage", e.getMessage());
+            }
+            request.setAttribute("action", request.getParameter("action"));
+        } else{
+            request.setAttribute("action", "insert");
+        }
+        defaultList(request);
+        return "employeeEntity-list";
+    }
+
+    public void defaultList(HttpServletRequest request){
+        Collection<EmployeeEntity> employeeEntityCollection = new ArrayList<EmployeeEntity>();
+        try{
+            employeeEntityCollection = employeeService.listEmployee();
+        } catch (Exception e){
+            //TODO Auto-generated catch block
+            e.printStackTrace();
+            request.setAttribute("errorMessage", e.getMessage());
+        }
+        request.setAttribute("employeeEntityCollection", employeeEntityCollection);
+    }
+
+    public EmployeeDto convertToDto(EmployeeEntity entity){
+        EmployeeDto dto = new EmployeeDto();
+        dto.setName(entity.getName());
+        dto.setId_number(entity.getId_number());
+        dto.setGender(entity.getGender());
+        dto.setBirth_date(entity.getBirth_date());
+        dto.setIs_delete(entity.getIs_delete());
+        return dto;
+    }
 }
--- src/main/java/v3aplikasi/model/dto/EmployeeDto.java
+++ src/main/java/v3aplikasi/model/dto/EmployeeDto.java
@@ -3,25 +3,81 @@ package v3aplikasi.model.dto;
 import v3aplikasi.model.entity.EmployeeEntity;
 import v3aplikasi.model.entity.PositionEntity;
 
+import java.util.Date;
 import java.util.List;
 
 public class EmployeeDto {
-    private List<PositionEntity> positionEntities;
-    private EmployeeEntity employeeEntity;
+//    private List<PositionEntity> positionEntities;
+//    private EmployeeEntity employeeEntity;
+//
+//    public List<PositionEntity> getPositionEntities() {
+//        return positionEntities;
+//    }
+//
+//    public void setPositionEntities(List<PositionEntity> positionEntities) {
+//        this.positionEntities = positionEntities;
+//    }
+//
+//    public EmployeeEntity getEmployeeEntity() {
+//        return employeeEntity;
+//    }
+//
+//    public void setEmployeeEntity(EmployeeEntity employeeEntity) {
+//        this.employeeEntity = employeeEntity;
+//    }
 
-    public List<PositionEntity> getPositionEntities() {
-        return positionEntities;
+    private Integer id;
+    private String name;
+    private Date birth_date;
+    private Integer id_number;
+    private Integer gender;
+    private Integer is_delete;
+
+    public Integer getId() {
+        return id;
     }
 
-    public void setPositionEntities(List<PositionEntity> positionEntities) {
-        this.positionEntities = positionEntities;
+    public void setId(Integer id) {
+        this.id = id;
     }
 
-    public EmployeeEntity getEmployeeEntity() {
-        return employeeEntity;
+    public String getName() {
+        return name;
     }
 
-    public void setEmployeeEntity(EmployeeEntity employeeEntity) {
-        this.employeeEntity = employeeEntity;
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Date getBirth_date() {
+        return birth_date;
+    }
+
+    public void setBirth_date(Date birth_date) {
+        this.birth_date = birth_date;
+    }
+
+    public Integer getId_number() {
+        return id_number;
+    }
+
+    public void setId_number(Integer id_number) {
+        this.id_number = id_number;
+    }
+
+    public Integer getGender() {
+        return gender;
+    }
+
+    public void setGender(Integer gender) {
+        this.gender = gender;
+    }
+
+    public Integer getIs_delete() {
+        return is_delete;
+    }
+
+    public void setIs_delete(Integer is_delete) {
+        this.is_delete = is_delete;
     }
 }
