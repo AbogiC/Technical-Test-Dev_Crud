40367763f639b3e8131064d2f6280a10d5909fc8
Abednego Bogi Christian <99196129+AbogiC@users.noreply.github.com>
2022-05-20 11:11:52 +0700

    edit file

--- src/main/java/v3aplikasi/dao/EmployeeDAO.java
+++ src/main/java/v3aplikasi/dao/EmployeeDAO.java
@@ -1,15 +1,13 @@
 package v3aplikasi.dao;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import v3aplikasi.model.entity.EmployeeEntity;
 
 import java.util.Collection;
-import java.util.List;
 
-public interface EmployeeDAO extends JpaRepository<EmployeeEntity, Integer> {
-//    public Collection<EmployeeEntity> listEmployee() throws Exception;
-//    public EmployeeEntity getEmployee(int idEmployee) throws Exception;
-//    public void insertEmployee(EmployeeEntity employeeEntity) throws Exception;
-//    public void updateEmployee(EmployeeEntity employeeEntity) throws Exception;
-//    public void deleteEmployee(EmployeeEntity employeeEntity) throws Exception;
+public interface EmployeeDAO {
+    public Collection<EmployeeEntity> listEmployee() throws Exception;
+    public EmployeeEntity getEmployee(int idEmployee) throws Exception;
+    public void insertEmployee(EmployeeEntity employeeEntity) throws Exception;
+    public void updateEmployee(EmployeeEntity employeeEntity) throws Exception;
+    public void deleteEmployee(EmployeeEntity employeeEntity) throws Exception;
 }
